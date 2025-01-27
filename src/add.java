9cb2fdd7caaea8f1886b600cb2420b2cde9f4f02
shenbaga lakshmi <shenbagalaks1996@gmail.com>
2025-01-27 18:40:51 +0530

    Initial commit

---
+++ src/add.java
@@ -0,0 +1,9 @@
+public class add {
+    public static void main(String[]args){
+        int a=49;
+        int u=90;
+        int v=989;
+        int c=a+u+v;
+        System.out.print("addition of the three numbers:"+ c);
+                }
+}
